8be216dea53f956fecafde5ac86a7fa6671cb238
skustra <kustraslawomir@gmail.com>
2018-12-06 19:23:04 +0100

    arc lines colors

--- networkspeed/src/main/java/slawomir/kustra/speedview/NetworkSpeedView.kt
+++ networkspeed/src/main/java/slawomir/kustra/speedview/NetworkSpeedView.kt
@@ -6,15 +6,11 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.graphics.SweepGradient
+import slawomir.kustra.utils.Gradients
 
 
 internal class NetworkSpeedView : View {
 
-    private var arcGradientColors =
-        intArrayOf(Color.parseColor("#FFFF76BD"), Color.parseColor("#FF71F0F0"))
-
-    private var linesColor =
-        intArrayOf(Color.parseColor("#FFF17FBC"), Color.parseColor("#FFF17FBC"), Color.parseColor("#FF47C0E2"))
 
     private val ovalPaint = Paint()
 
@@ -44,7 +40,7 @@ internal class NetworkSpeedView : View {
         val cy = h / 2f
         ovalBackgroundShape.set(cx - s2, cy - s2, cx + s2, cy + s2)
         val positions = floatArrayOf(0.0f, 0.75f)
-        val shader = SweepGradient(cx, cy, arcGradientColors, positions)
+        val shader = SweepGradient(cx, cy, Gradients.arcGradientColors, positions)
         val m = Matrix()
         m.setRotate(135f, cx, cy)
         shader.setLocalMatrix(m)
@@ -60,8 +56,7 @@ internal class NetworkSpeedView : View {
         ovalPaint.strokeCap = Paint.Cap.ROUND
 
         arcLines.alpha = 0.6.toInt()
-        arcLines.color =Color.parseColor("#65FFFFFF")
-        arcLines.strokeWidth = 6f
+        arcLines.strokeWidth = 7f
         arcLines.strokeCap = Paint.Cap.ROUND
     }
 
@@ -84,22 +79,32 @@ internal class NetworkSpeedView : View {
         Log.e("NSV", "left: $left | top: $top | right: $right | bottom: $bottom")
         canvas.drawArc(ovalBackgroundShape, 160f, 220f, false, ovalPaint)
 
+        val colors = Gradients.linesColor
+        colors.reverse()
         canvas.save()
+        var counter = 46
+        for (angle in 0..360) {
+            if ((angle in 0..110 && angle % 5 == 0) || (angle in 250..360 && angle % 5 == 0)) {
 
-        for (i in 0..360) {
-            if ((i in 0..110 && i % 5 == 0) || (i in 250..360 && i % 5 == 0)) {
-
-                if (i % 10 == 0) {
-                    arcLines.strokeWidth = 4f
+                Log.e("NSV:", "angle: $angle")
+                if (angle % 10 == 0) {
+                    arcLines.strokeWidth = 5f
                 }
 
-                val angle = Math.toRadians(i.toDouble())
+                arcLines.color = colors[counter]
+                counter--
+
+                val angle = Math.toRadians(angle.toDouble())
                 val startX = centerX + (radius + 65) * Math.sin(angle)
                 val startY = centerY - (radius + 65) * Math.cos(angle)
                 val stopX = centerX + ((radius + 65) - scaleMarkSize) * Math.sin(angle)
                 val stopY = centerY - ((radius + 65) - scaleMarkSize) * Math.cos(angle)
-                Log.e("NSV:", "draw line on angle: $i, startX: $startX  startY: $startY  stopX: $stopX  stopY: $stopY")
+                Log.e(
+                    "NSV:",
+                    "draw line on angle: $angle, startX: $startX  startY: $startY  stopX: $stopX  stopY: $stopY"
+                )
 
+                Log.e("NSV:", "counter : $counter")
                 canvas.drawLine(startX.toFloat(), startY.toFloat(), stopX.toFloat(), stopY.toFloat(), arcLines)
             }
         }
---
+++ networkspeed/src/main/java/slawomir/kustra/utils/Gradients.kt
@@ -0,0 +1,61 @@
+package slawomir.kustra.utils
+
+import android.graphics.Color
+
+open class Gradients {
+    companion object {
+
+        var arcGradientColors =
+            intArrayOf(Color.parseColor("#FFFF76BD"), Color.parseColor("#FF71F0F0"))
+
+        var linesColor =
+            intArrayOf(
+                Color.parseColor("#949AC6"),
+                Color.parseColor("#8F9DC7"),
+                Color.parseColor("#8BA1C9"),
+                Color.parseColor("#86A4CA"),
+                Color.parseColor("#82A7CB"),
+                Color.parseColor("#7DAACC"),
+                Color.parseColor("#79AECE"),
+                Color.parseColor("#74B1CF"),
+                Color.parseColor("#70B4D0"),
+                Color.parseColor("#6BB7D1"),
+                Color.parseColor("#67BBD3"),
+                Color.parseColor("#63BED4"),
+                Color.parseColor("#5EC1D5"),
+                Color.parseColor("#5AC4D6"),
+                Color.parseColor("#55C8D8"),
+                Color.parseColor("#51CBD9"),
+                Color.parseColor("#4CCEDA"),
+                Color.parseColor("#48D1DB"),
+                Color.parseColor("#43D5DD"),
+                Color.parseColor("#3FD8DE"),
+                Color.parseColor("#3ADBDF"),
+                Color.parseColor("#36DEE0"),
+                Color.parseColor("#32E2E2"), Color.parseColor("#FF4DA9"),
+                Color.parseColor("#FA50AA"),
+                Color.parseColor("#F653AB"),
+                Color.parseColor("#F156AC"),
+                Color.parseColor("#ED59AD"),
+                Color.parseColor("#E85DAF"),
+                Color.parseColor("#E460B0"),
+                Color.parseColor("#DF63B1"),
+                Color.parseColor("#DB66B2"),
+                Color.parseColor("#D66AB4"),
+                Color.parseColor("#D26DB5"),
+                Color.parseColor("#CD70B6"),
+                Color.parseColor("#C973B7"),
+                Color.parseColor("#C577B9"),
+                Color.parseColor("#C07ABA"),
+                Color.parseColor("#BC7DBB"),
+                Color.parseColor("#B780BC"),
+                Color.parseColor("#B384BE"),
+                Color.parseColor("#AE87BF"),
+                Color.parseColor("#AA8AC0"),
+                Color.parseColor("#A58DC1"),
+                Color.parseColor("#A191C3"),
+                Color.parseColor("#9C94C4"),
+                Color.parseColor("#9897C5")
+            )
+    }
+}
